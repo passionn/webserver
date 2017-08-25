ec133e7d76c5e0428fc6b5892df2ab4c0c0dd1dc
zhangpan <yzhangpan@gmail.com>
2017-08-25 22:02:18 +0800

    update

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 import Koa from 'koa'
+import path from 'path'
+import views from 'koa-views'
 import logger from 'koa-logger'
+import koaStaticPlus from 'koa-static-plus'
+
 
 const app = new Koa()
 
 app.use(logger())
 
+console.log( path.join(__dirname, '../public'))
+
+app.use(koaStaticPlus(path.join(__dirname, '../public')),{
+   pathPrefix:''
+})
+
+// views
+app.use(views(path.join(__dirname, '../views'), {
+  extension: 'ejs'
+}))
 
 // response
-app.use(async (ctx) => {
-  ctx.body = 'Hello World!'
+// response router
+app.use(async (ctx, next) => {
+  await require('./routes').routes()(ctx, next)
 })
 
 app.listen(3000, () => console.log('server started 3000'))
