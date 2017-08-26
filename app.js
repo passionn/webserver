d6e1b4a7770182c502221f054a408fccafdfec63
yzhangpan <yzhangpan@gmail.com>
2017-08-26 13:29:36 +0800

    update

--- app.js
+++ app.js
@@ -3,28 +3,27 @@ import path from 'path'
 import views from 'koa-views'
 import logger from 'koa-logger'
 import koaStaticPlus from 'koa-static-plus'
+import router from './routes'
 
 
 const app = new Koa()
 
 app.use(logger())
 
-console.log( path.join(__dirname, '../public'))
+console.log( path.join(__dirname, './public'))
 
-app.use(koaStaticPlus(path.join(__dirname, '../public')),{
+app.use(koaStaticPlus(path.join(__dirname, './public')),{
    pathPrefix:''
 })
 
 // views
-app.use(views(path.join(__dirname, '../views'), {
+app.use(views(path.join(__dirname, './views'), {
   extension: 'ejs'
 }))
 
-// response
 // response router
-app.use(async (ctx, next) => {
-  await require('./routes').routes()(ctx, next)
-})
+app.use(router.routes())
+	.use(router.allowedMethods());
 
 app.listen(3000, () => console.log('server started 3000'))
 
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
-index.js
+import indexCtrl from '../controllers/indexCtrl.js'
+import Router from 'koa-router'
+
+const router =new Router()
+
+router.get('/', indexCtrl)
+
+export default router
