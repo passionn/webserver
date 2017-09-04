86fca0921a6e3f1c5376e217d8fa4a883518e75a
zhangpan <yzhangpan@gmail.com>
2017-09-04 19:08:10 +0800

    update

---
+++ db/db.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb://localhost/blog');
+
+let db = mongoose.connection;
+
+db.on('error', function(){
+    console.log('数据库连接出错！');
+});
+db.on('open', function(){
+    console.log('数据库连接成功！');
+});
+
+//声明schema
+const articleSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    content: String,
+    description:String,
+    create_time: Date
+});
+
+//根据schema生成model
+const model = {
+    Article: mongoose.model('Article', articleSchema)
+};
+
+export default model
